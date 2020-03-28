9cd30f62f3368071d5ce4d19c87f4c828b39985c
Gideon Dembovsky <gidemsky26@gmail.com>
2020-03-28 22:56:27 +0300

    debug print fix seperator changed to big and small

--- GoogleTranslateAPI.py
+++ GoogleTranslateAPI.py
@@ -12,7 +12,7 @@ Class abilities:
 """
 
 import six
-import Utils as util
+import Utils as Tool
 from google.cloud import translate_v2 as translate
 
 
@@ -28,8 +28,10 @@ class GoogleTranslateAPI(object):
         :param to_language: The language to translate results into.
         :param print_result: If we want to print the result
         :param model: 'base' or 'nmt'. By default it using NMT.
-        :return: The translated to the destination language string
+        :return: list. The translated to the destination language
         """
+
+        translated_lines = list()
 
         # Modifies the text to utf-8 in case it doesn't
         if isinstance(source_text, six.binary_type):
@@ -38,17 +40,25 @@ class GoogleTranslateAPI(object):
         # Translates the source text.
         translated_text = self.translate_client.translate(values=source_text, target_language=to_language, model=model)
 
+        if isinstance(source_text, six.string_types):
+            translated_text = [translated_text]
+
         # for debug purpose
         if print_result:
-            util.separate_debug_print("Start Translation Result")
-            print(u'Text to translate: {}'.format(
-                translated_text['input']))
-            print(u'The translation: {}'.format(
-                translated_text['translatedText']))
-            print(u'Detected source language: {}'.format(
-                translated_text['detectedSourceLanguage']))
-            util.separate_debug_print("End Translation Result")
-        return translated_text['translatedText']
+            Tool.separate_debug_print_big(title="Start Translation Result")
+            for cur_translated, i in zip(translated_text, range(translated_text.__len__())):
+                Tool.separate_debug_print_small(title='Translate Line Number: ' + i.__str__())
+                print(u'Text to translate: {}'.format(
+                    cur_translated['input']))
+                print(u'The translation: {}'.format(
+                    cur_translated['translatedText']))
+                print(u'Detected source language: {}'.format(
+                    cur_translated['detectedSourceLanguage']))
+                translated_lines.append(cur_translated['translatedText'])
+                Tool.separate_debug_print_small(title='Translation result end')
+            Tool.separate_debug_print_big(title="Line Translation End")
+
+        return translated_lines  # TODO: We Hve to decide how do we what the return value to be
 
     def supp_language(self, language=None):
         """
@@ -58,10 +68,10 @@ class GoogleTranslateAPI(object):
         """
         all_supported_language = self.translate_client.get_languages(target_language=language)
 
-        util.separate_debug_print("Start the supported languages")
+        Tool.separate_debug_print_big("Start the supported languages")
         for cur_language in all_supported_language:
             print(u'{name} ({language})'.format(**cur_language))
-        util.separate_debug_print("End the supported languages")
+        Tool.separate_debug_print_big("End the supported languages")
 
     def language_detection(self, source_text, print_result=False):
         """
@@ -75,16 +85,30 @@ class GoogleTranslateAPI(object):
 
         # for debug reason
         if print_result:
+            Tool.separate_debug_print_big(title='Start Detection Results')
             print('Text: {}'.format(source_text))
             print('Confidence: {}'.format(language_result['confidence']))
             print('Language: {}'.format(language_result['language']))
+            Tool.separate_debug_print_big(title='End Detection Results')
 
         return language_result['language']
 
 
 if __name__ == '__main__':
+
     test_text = "Hey, My name is Gidi. I like movies, sex, and Disco-Dancing. This is test for the translation"
     test_text2 = "היי, שוב בדיקה!"
+    test_text3 = "test"
+
+    string_text = list()
+
+    string_text.append("this is text number one")
+    string_text.append("I can't wait to finish our project")
+    string_text.append("By the way, I am hae every one")
+    string_text.append("this is text number two")
+    string_text.append("And what about us?!")
+    string_text.append("We will win this!!!")
+
     google = GoogleTranslateAPI()
     google.supp_language()
 
@@ -101,3 +125,9 @@ if __name__ == '__main__':
 
     result = google.translate(test_text2, 'ar', True)
     print(result)
+
+    result = google.translate(string_text, 'iw', True)
+    print(result)
+
+    google.language_detection(test_text2, True)
+    google.language_detection(test_text3, True)
--- Utils.py
+++ Utils.py
@@ -1,4 +1,8 @@
 
-def separate_debug_print(title):
+def separate_debug_print_big(title):
     print('\n------------------------------' + title + '------------------------------\n')
+
+
+def separate_debug_print_small(title):
+    print('\n---------------' + title + '---------------')
 
