f5b36951c25525e7968e1337cbcf5933a159d887
Oriyah Aharon <t-orahar@microsoft.com>
2020-06-02 10:58:02 +0300

    models for election sentiment analysis

--- Utils.py
+++
@@ -1,8 +0,0 @@
-
-def separate_debug_print_big(title):
-    print('\n------------------------------' + title + '------------------------------\n')
-
-
-def separate_debug_print_small(title):
-    print('\n---------------' + title + '---------------')
-
